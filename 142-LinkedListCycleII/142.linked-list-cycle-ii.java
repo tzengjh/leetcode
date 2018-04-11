710c72f84342f127e410bc82d07431710ac9d473
tzengjh <zengjinghong@hotmail.com>
2018-04-11 14:31:29 +0800

    file cache

---
+++ 142-LinkedListCycleII/142.linked-list-cycle-ii.java
@@ -0,0 +1,40 @@
+/*
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (30.62%)
+ * Total Accepted:    143.7K
+ * Total Submissions: 469.4K
+ * Testcase Example:  '[]\nno cycle'
+ *
+ * 
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * 
+ * 
+ * 
+ * Note: Do not modify the linked list.
+ * 
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+    }
+}
---
+++ 153-FindMinimumInRotatedSortedArray/153.find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,25 @@
+/*
+ * [153] Find Minimum in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (40.82%)
+ * Total Accepted:    190.4K
+ * Total Submissions: 466.4K
+ * Testcase Example:  '[1]'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * Find the minimum element.
+ * 
+ * You may assume no duplicate exists in the array.
+ */
+class Solution {
+    public int findMin(int[] nums) {
+        
+    }
+}
