f8de31e975fbd6dfdf16a77e8eeec78465d76c39
jinghong <zengjinghong@hotmail.com>
2018-10-25 16:59:46 +0800

    Revise solution of add-binary

--- 067-AddBinary/67.add-binary.java
+++ 067-AddBinary/67.add-binary.java
@@ -1,4 +1,33 @@
+/*
+ * [67] Add Binary
+ *
+ * https://leetcode.com/problems/add-binary/description/
+ *
+ * algorithms
+ * Easy (36.14%)
+ * Total Accepted:    241.2K
+ * Total Submissions: 667.4K
+ * Testcase Example:  '"11"\n"1"'
+ *
+ * Given two binary strings, return their sum (also a binary string).
+ * 
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * 
+ */
 class Solution {
+
 	public String addBinary(String a, String b) {
 		if(a == null && b == null)
 			throw new IllegalArgumentException("Both of the Strings are null !");
@@ -17,21 +46,15 @@ class Solution {
 			if(ta == '0' && tb == '0'){
 				c = t + c;
 				t = '0';
-
 			}else if(ta == '1' && tb == '1'){
 				c = t + c;
 				t = '1';
-
 			}else{
 				c = t =='1'?'0' + c : '1' + c;               
-
 			}
-
 		}
 		if(t == '1')
 			c = t + c;
 		return c;
-
 	}
-
 }
